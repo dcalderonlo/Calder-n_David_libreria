3587ea4aff3e02c1e32cc67a660433ff59f425a3
David Calderon <dcalderonlo@outlook.com>
2023-11-10 08:35:36 -0400

    Modifying banchs checks

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -5,13 +5,11 @@ def call(boolean abortPipeline = false, boolean evaluateQualityGate = false) {
     def sonarenv = [:]
     
     timeout(time: 5, unit: 'MINUTES') {
-        if (evaluateQualityGate) {
-            if (evaluateQualityGate) {
-                echo "Quality Gate no aprobado. El pipeline será abortado."
-                currentBuild.result = 'FAILURE'
-                if (abortPipeline) {
-                    error("El pipeline ha sido abortado según la configuración.")
-                }
+        if (evaluateQualityGate || shouldAbortPipeline()) {
+            echo "Quality Gate no aprobado. El pipeline será abortado."
+            currentBuild.result = 'FAILURE'
+            if (abortPipeline) {
+                error("El pipeline ha sido abortado según la configuración.")
             }
         }
     }
